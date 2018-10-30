24e02ec2adde68e1feb6770583dc6815da4500d1
Brian <tjtbrian@gmail.com>
2018-10-30 10:12:48 +0800

    completed server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const app = require("./app");
+const logger = require("./logger")
 
-const server = app.listen(process.env.PORT || 3000, function() {
+const server = app.listen(process.env.PORT || 3001, function() {
+  // @ts-ignore
   logger.info("Listening on port " + server.address().port);
 });
